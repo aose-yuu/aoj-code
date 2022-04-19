050a01831333d03c93d8802dbe576df3ebc46588
aose-yuu <aose.developer@gmail.com>
2022-04-20 00:43:01 +0900

    [add]answer_code !-D

--- topic-1/1-D/1-d.cpp
+++ topic-1/1-D/1-d.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 int main() {
+    int x;
+    cin >> x;
+    int h = x / 3600;
+    int m = x % 3600 / 60;
+    int s = x % 60;
+
+    cout << h << ":" << m << ":" << s << endl;
     return 0;
 }
